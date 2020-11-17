f1e7fe0d1f1dbd475fd3a6b8700e50274dc9f553
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-11-17 09:54:06 +0000

    v2.0

--- ESEMPIOmongoDB/routes/movies.js
+++ ESEMPIOmongoDB/routes/movies.js
@@ -4,21 +4,22 @@ var router = express.Router();
 const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/movie_from_title/:title', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    title = req.params.title;
     const uri = 'mongodb+srv://alen_bicanic:KPgZbP7MWt061Quk@cluster1.8ojbw.mongodb.net/Cluster1?retryWrites=true&w=majority'
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
         const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
-        // perform actions on the collection object
-        collection.find().limit(10).toArray((err, result) => {
+        // eseguo una find sulla collection
+        collection.find({ 'title': `${title}` }).toArray((err, result) => {
             if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-            else console.log(result);
-            res.send(result);
+            else res.send(result);
             client.close(); //Quando ho terminato la find chiudo la sessione con il db
         }); //Eseguo la query e passo una funzione di callback
 
     });
-    
 });
+
 
 module.exports = router;
