6d4de363e9b65c39b47eca35de710ca5a33fe429
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-11-20 12:44:32 +0000

    v3.0

--- ESEMPIOmongoDB/routes/movies.js
+++ ESEMPIOmongoDB/routes/movies.js
@@ -20,6 +20,55 @@ router.get('/movie_from_title/:title', function (req, res, next) {
 
     });
 });
+router.get('/list/:num', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    let num = parseInt(req.params.num);
+    const uri = 'mongodb+srv://alen_bicanic:KPgZbP7MWt061Quk@cluster1.8ojbw.mongodb.net/Cluster1?retryWrites=true&w=majority'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // eseguo una find sulla collection
+        collection.find().limit(num).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+});
+
+router.get('/year/:num', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    let num = parseInt(req.params.num);
+    const uri = 'mongodb+srv://alen_bicanic:KPgZbP7MWt061Quk@cluster1.8ojbw.mongodb.net/Cluster1?retryWrites=true&w=majority'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // eseguo una find sulla collection
+        collection.find({year:num}).limit(num).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+});
+router.get('/rating/:num', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    let num = parseInt(req.params.num);
+    const uri = 'mongodb+srv://alen_bicanic:KPgZbP7MWt061Quk@cluster1.8ojbw.mongodb.net/Cluster1?retryWrites=true&w=majority'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // eseguo una find sulla collection
+        collection.find({'imdb.rating': num}).limit(num).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+});
 
 
 module.exports = router;
